8f91481688cc933f6bec6e0d51ff49f129dfbc69
James.T <torresjames48@yahoo>
2019-03-07 10:25:36 -0600

    html and css completed for now

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// var/objects for each charecter
+var Goku= {
+    Name: "Goku",
+    Hp: 200,
+    Atk: 5,
+    CounterAtk: 5
+}
+
+var Vegta= { 
+    Name: "Vegeta",
+    Hp: 180,
+    Atk: 10,
+    CounterAtk: 10
+}
+
+var Krillin ={
+    Name: "Krillin",
+    Hp: 75,
+    Atk: 10,
+    CounterAtk: 10
+}
+var Nappa ={
+    Name: "Krillin",
+    Hp: 100,
+    Atk: 20,
+    CounterAtk: 20,
+}
+// on click actions and events
